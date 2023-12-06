a8a125b4fc1c7ce2d462b487b5c65bf5ecd0b3b5
FranScicluna <francescoscicluna@gmail.com>
2023-12-06 10:04:11 +0100

    commit

--- app/public/wp-content/themes/mcastica/single.php
+++ app/public/wp-content/themes/mcastica/single.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 <?php 
+if(have_posts()) :while(have_posts()) : the_post();
     $author_id = get_the_author_meta("ID");
     $author_posts = get_the_author_posts();
     $author_display = get_the_author();
     $author_posts_url = get_author_posts_url($author_id);
     $author_desc = get_the_author_meta("user_description");
     $author_url = get_the_author_meta("user_url");
+    $post_datetime = get_the_date();
 ?>
 
 <div class="container">
@@ -13,17 +15,11 @@
         <div class="col-3">
             <div class="row sidebarFlex mt-4 p-4">
                 <div class="col-1">
-                    <p>admin</p>
+                <?php echo "<p><a href='{$author_posts_url}'>{$author_display}</a></p>";?>
                 </div>
                 <div class="col-2">
                     <?php
-                        if($author_posts > 1){
-                            $post_word = "Posts";
-                        }
-                        else{
-                            $posts_word = "Post";
-                        }
-                        echo "<p><a href='{$author_posts_url}'>{$author_posts} {$post_word}</a></p>";
+                        echo "<p> {$post_datetime} </p>"
                     ?>
                 </div>
             </div>
@@ -31,7 +27,7 @@
         </div>
         <div class="col-9">
             <main>
-                <?php if(have_posts()) :while(have_posts()) : the_post();?>
+                
 
                     <article <?php post_class("mt-4 p-4");?>>
                         <h1><?php the_title()?></h1>
@@ -39,44 +35,29 @@
                         <div><?php the_content(); ?></div>
 
                         <?php 
-                            $author_id = get_the_author_meta("ID");
-                            $author_posts = get_the_author_posts();
-                            $author_display = get_the_author();
-                            $author_posts_url = get_author_posts_url($author_id);
-                            $author_desc = get_the_author_meta("user_description");
-                            $author_url = get_the_author_meta("user_url");
+                            // $author_id = get_the_author_meta("ID");
+                            // $author_posts = get_the_author_posts();
+                            // $author_display = get_the_author();
+                            // $author_posts_url = get_author_posts_url($author_id);
+                            // $author_desc = get_the_author_meta("user_description");
+                            // $author_url = get_the_author_meta("user_url");
                         ?>
 
                         <div class="bg-dark">
                             <div><?php echo get_avatar($author_id,200);?></div>
                         </div>
-
-                        <?php
-                            if($author_url){
-                                echo "<p><a href='{$author_url}'>{$author_display}</a></p>";
-                            }
-                            else{
-                                echo "<p>{$author_display}</p>";
-                            }
-
-                            if($author_desc){
-                                echo "<p>{$author_desc}</p>";
-                            }
-                        ?>
-
-
                     </article>
 
                     
 
-                <?php 
-                    endwhile; 
-                    endif; 
-                ?>
+                
             </main>
         </div>
 
     </div>
 </div>
-
+<?php 
+                    endwhile; 
+                    endif; 
+                ?>
 <?php get_footer(); ?>
