8d3e0724ddb2fec25783b4a4a98b73e1cefd6590
FranScicluna <francescoscicluna@gmail.com>
2023-12-06 11:49:31 +0100

    courses

--- app/public/wp-content/themes/mcastica/single.php
+++ app/public/wp-content/themes/mcastica/single.php
@@ -14,13 +14,17 @@ if(have_posts()) :while(have_posts()) : the_post();
     <div class="row">
         <div class="col-3">
             <div class="row sidebarFlex mt-4 p-4">
-                <div class="col-1">
+                <div>
                 <?php echo "<p><a href='{$author_posts_url}'>{$author_display}</a></p>";?>
+                <hr>
+                <p class="newsSideText">Author Name</p>
                 </div>
-                <div class="col-2">
+                <div>
                     <?php
                         echo "<p> {$post_datetime} </p>"
                     ?>
+                    <hr>
+                    <p class="newsSideText">Date Published</p>
                 </div>
             </div>
             <?php //get_sidebar(); ?>
@@ -33,15 +37,6 @@ if(have_posts()) :while(have_posts()) : the_post();
                         <h1><?php the_title()?></h1>
 
                         <div><?php the_content(); ?></div>
-
-                        <?php 
-                            // $author_id = get_the_author_meta("ID");
-                            // $author_posts = get_the_author_posts();
-                            // $author_display = get_the_author();
-                            // $author_posts_url = get_author_posts_url($author_id);
-                            // $author_desc = get_the_author_meta("user_description");
-                            // $author_url = get_the_author_meta("user_url");
-                        ?>
 
                         <div class="bg-dark">
                             <div><?php echo get_avatar($author_id,200);?></div>
