cdd2a4a87d836ae36ef64f11a972ad217639fe24
FranScicluna <francescoscicluna@gmail.com>
2023-12-08 00:16:54 +0100

    Building Pages

---
+++ app/public/wp-content/themes/mcastica/home.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
+			<?php 
+				$args = array('section_title' => 'Home');
+				get_template_part("template-parts/loop", null, $args);
+			?>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
